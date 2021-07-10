511511cfb319f3bf0b22bb8f142a824ab789002f
Tanjim <tanjimahammed776@gmail.com>
2021-07-10 18:33:09 +0600

    all roject

---
+++ Ch-1_programs/ch-2_programs/math_operation.py
@@ -0,0 +1,5 @@
+a = 3
+h = b = "5"
+int(h)
+c = a + b
+print(c)
---
+++ Ch-1_programs/exercise/find_big_num.py
@@ -0,0 +1,8 @@
+n1 = input("Please enter a number: ")
+n1 = int(n1)
+n2 = input("Please enter another number: ")
+n2 = int(n2)
+if n2 < n1:
+    print("first number is big: ", n1)
+else:
+    print("second  number is big: " , n2)
