9d6d67ff2242537718d9146902ea0425b9710022
Tanjim <tanjimahammed776@gmail.com>
2021-07-15 19:09:19 +0600

    facebook program

---
+++ Ch-1_programs/ch-2_programs/arithmatic.py
@@ -0,0 +1,5 @@
+number1 = input("Pleasee enter an integer and press enter: ")
+number2 = input("Please type another integer and press enter: ")
+number1 = int(number1)
+number2 = int(number2)
+print('Number1 ** Number2 =', number1 ** number2)
---
+++ Ch-1_programs/ch-3_programs/logic.py
@@ -0,0 +1,5 @@
+my_money  = 30
+rickshaw_fare = 40
+if my_money < rickshaw_fare:
+    print("My money is big")
+    input("Please enter a bu")
